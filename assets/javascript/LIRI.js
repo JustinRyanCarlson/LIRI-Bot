fd91cfb29c59769596c18d1f3ddeeafa8f7c4bd3
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-12-17 09:41:52 -0600

    adds comments

--- assets/javascript/LIRI.js
+++ assets/javascript/LIRI.js
@@ -1,3 +1,4 @@
+// Various variable using require
 var twitterKeysFile = require('./keys.js');
 var twitterKeysObject = twitterKeysFile.twitterKeys;
 var spotify = require('spotify');
@@ -10,19 +11,27 @@ var client = new Twitter({
 });
 var request = require('request');
 var fs = require('fs');
+// Declares the inputCommand variable as the 3rd argument passed in from
+// the command line
 var inputCommand = process.argv[2];
+// Declares the inputTerm variable as everything after the index of 2 (stores as an array)
+// then joins that array back together with spaces seperating each index
 var inputTerm = process.argv.slice(3).join(' ');
 
 
-
+// Calls the logger function so the users input is in the log.txt file
 logger('User input: ' + inputCommand + ' ' + inputTerm);
+// Calls the runCommand function and passes in the users input from the command line
 runCommand(inputCommand, inputTerm);
 
 
-
+// Function that takes in the command and term variable to determine which case to run and 
+// what to pass to the API
 function runCommand(command, term) {
     switch (command) {
 
+        // If the command passed in is 'my-tweets' this case is ran and prints/logs the 20
+        // most recent tweets from my Twitter with the time they were tweeted
         case 'my-tweets':
             var params = { screen_name: 'Justinc101011' };
             client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -37,12 +46,18 @@ function runCommand(command, term) {
             });
             break;
 
+            // If the command passed in is 'spotify-this-song' this case is ran
         case 'spotify-this-song':
+            // Makes a request to the spotify api for the track the user searched
             spotify.search({ type: 'track', query: term }, function(err, data) {
                 if (err) {
                     logger('Error occurred: ' + err);
                     return;
                 }
+                // If there are no items for the search (undefined) default to 'The Sign' by Ace of Bass
+                // I hard-coded this because even if i searched 'The Sign' with the spotify API, it didn't
+                // return what I was looking for first. I could have pointed to to an index, but this songs
+                // index may change when a new song entitled 'The Sign' is uploaded
                 if (typeof data.tracks.items[0] == 'undefined') {
                     logger('--------------------------------------------------------------------------------------------------');
                     logger('Artist: Ace of Bass');
@@ -51,11 +66,16 @@ function runCommand(command, term) {
                     logger('Album: The Sign (US Album) [Remastered]');
                     logger('==================================================================================================');
                 } else {
+                    // Lines 70-84 loop over each result returned from the spotify API and call the logger function
+                    // for the information we want. If spotify returns less than 20 results, the for loop will break
+                    // out so we dont get a undefined type error
                     for (var i = 0; i < 20; i++) {
                         if (typeof data.tracks.items[i] == 'undefined') {
                             break;
                         }
                         var artists = [];
+                        // Nested for loop needed to retrieve all the artists for a song since there
+                        // can be more than one
                         for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
                             artists.push(data.tracks.items[i].artists[j].name);
                         }
@@ -70,6 +90,10 @@ function runCommand(command, term) {
             });
             break;
 
+            // If the command passed in is 'movie-this' this case is ran and prints/logs information
+            // about the movie
+            // If no movie information is returned from OMDB the runCommand function is called and
+            // passes in the information needed to run movie-this for the movie Mr. Nobody (dafault)
         case 'movie-this':
             var OMDBLink = 'http://www.omdbapi.com/?t=' + term + '&y=&plot=short&tomatoes=true&r=json';
             request(OMDBLink, function(error, response, body) {
@@ -102,6 +126,8 @@ function runCommand(command, term) {
             });
             break;
 
+            // If the command passed in is 'do-what-it-says' this case is ran and reads the file random.txt
+            // then runs whatever is in the file as if the user input it from the command line
         case 'do-what-it-says':
             fs.readFile('../../random.txt', 'utf8', function(err, data) {
                 if (err) {
@@ -115,6 +141,8 @@ function runCommand(command, term) {
             });
             break;
 
+            // If no other case requirements are met, a valid command was not used and this case
+            // prints/logs the information needed to use a valid case to the user
         default:
             logger('Please enter a valid command');
             logger('Valid Commands are: my-tweets, spotify-this-song, movie-this, or do-what-it-says');
@@ -122,8 +150,10 @@ function runCommand(command, term) {
 }
 
 
-
+// Function that takes in what was going to be console.logged and still console.logs it then
+// appends it to the log.txt file
 function logger(stdout) {
+    // Gets the current time for the timestamp
     var currentTime = Date.now();
     console.log(stdout);
     fs.appendFile('../../log.txt', currentTime + '           ' + stdout + '\n', function(err) {
